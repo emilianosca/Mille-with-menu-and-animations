65b2b561a7930cb2e971faa9cd14c41640698979
Emiliano Sánchez <emiliano_sanchez@criptext.com>
2022-02-13 13:42:56 -0600

    landing nav and menu working fine

--- apps/animationApp.js
+++ apps/animationApp.js
@@ -1,7 +1,7 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-tl.to(".text", { y: "0%", duration: 4, stagger: 0.25 });
-// tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 }); //slider
+tl.to(".text", { y: "0%", duration: 3, stagger: 0.25 });
+tl.to(".slider", { y: "-100%", duration: 1, delay: 0.5 }); //slider
 tl.to(".intro", { y: "-100%", duration: .5 }, "-=1");
 tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: 1 });
 tl.fromTo(".big-text", { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1");
