c23d95c79c8726b6c9c5dd1dd93d072649fd2647
Emiliano Sánchez <emiliano_sanchez@criptext.com>
2022-02-22 20:16:09 -0600

    menu, footer & mainVar working 100% good

--- apps/navApp.js
+++ apps/navApp.js
@@ -1,6 +1,6 @@
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
-    console.log("Clicked menu");
+    // console.log("Clicked menu");
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
@@ -9,19 +9,19 @@ $('.navTrigger').click(function () {
 // funcion para cambiar color al llegar a ciertos pixeles
 
 
-if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+// if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
     // true for mobile device
-    console.log("mobile");
+    // console.log("mobile");
 
         // console.log
     // document.getElementById('.btn-container').style.gridTemplateColumns = "50% 50%";
     // document.getElementById('.btn-container').style.fontsize = "1rem"
     // document.getElementsByClassName('.btn-container').style.backgroundColor = "black";
     // document.getElementById('.logo').style.width = "10.5rem"
-  }else{
+  // }else{
     // false for not mobile device
-    console.log("not mobile device");
-  }
+    // console.log("not mobile device");
+  // }
 
 
 
